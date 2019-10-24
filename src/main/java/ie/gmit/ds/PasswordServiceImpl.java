7f721e8dfb8c0aa61cfaa70abea7d6c4e403ee6c
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-10-24 18:05:34 +0100

    Adding PasswordServiceimpl class with Validate method and modifying Password.java

--- src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,5 +1,31 @@
 package ie.gmit.ds;
 
+import com.google.protobuf.BoolValue;
+
+import io.grpc.stub.StreamObserver;
+
 public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase{
+	
+	// as per project spec 
+	@Override
+	public void validate(ValidateRequest request ,StreamObserver<BoolValue> reponse) {
+		
+		try {
+				boolean correctPassword = Passwords.isExpectedPassword(request.getPassword(), request.getSalt(), request.getHashedPassword());
+				
+				System.out.print("Valid" + correctPassword);
+				
+				if(correctPassword == true) {
+					
+				}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		
+	}
 
 }
