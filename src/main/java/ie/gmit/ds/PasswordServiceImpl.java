4c2052d713c8d0cf97ba4028184e35ced0f37465
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-10-24 18:43:56 +0100

    Adding hash Method to PasswordServerImpl

--- src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,6 +1,7 @@
 package ie.gmit.ds;
 
 import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 
 import io.grpc.stub.StreamObserver;
 
@@ -27,6 +28,29 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
 			e.printStackTrace();
 		}
 
+	}// end validate 
+	
+	@Override
+	public void hash(HashRequest resquest, StreamObserver<HashResponse> response) {
+		try {
+			
+			byte[] salt = Passwords.getNextSalt();
+			byte[] hashPassword = Passwords.hash(resquest.getPassword().toCharArray(), salt);
+			
+			ByteString saltByteString = ByteString.copyFrom(salt);
+			ByteString hashPasswordByteString = ByteString.copyFrom(hashPassword);
+			
+			System.out.println(saltByteString);
+			System.out.println(hashPasswordByteString);
+			
+			response.onNext(HashResponse.newBuilder().setUserID(resquest.getUserID()).setHashedPassword(hashPasswordByteString).setSalt(saltByteString).build());
+			response.onCompleted();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
+	
+	
 
 }
