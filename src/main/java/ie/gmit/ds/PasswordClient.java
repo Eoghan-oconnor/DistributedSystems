ace369e934449681cebaa73792458632b826e12d
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-10-25 17:40:46 +0100

    adding passwodClient

--- src/main/java/ie/gmit/ds/PasswordClient.java
+++ src/main/java/ie/gmit/ds/PasswordClient.java
@@ -1,5 +1,27 @@
 package ie.gmit.ds;
 
+import java.util.logging.Logger;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class PasswordClient {
+	
+	// logger used to log message for application component
+	private static final Logger logger = Logger.getLogger(PasswordClient.class.getName());
+	
+	private final ManagedChannel channel;
+	private final PasswordServiceGrpc.PasswordServiceStub asyncService;
+	private final PasswordServiceGrpc.PasswordServiceBlockingStub syncService;
+	
+	public PasswordClient(String host, int port) {
+		
+		channel = ManagedChannelBuilder.forAddress(host,port).usePlaintext().build();
+		
+		syncService = PasswordServiceGrpc.newBlockingStub(channel);
+		asyncService = PasswordServiceGrpc.newStub(channel);
+	}
+	
+	
 
 }
